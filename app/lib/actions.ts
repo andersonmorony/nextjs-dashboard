62d81ca6b5a889e5dcdc4a2151087099c480cbb4
Anderson Ramos <andersonmoroni92@gmail.com>
2026-05-25 20:27:34 -0300

    fix erro

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -11,15 +11,30 @@ const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 const FormSchema = z.object({
     id: z.string(),
-    customerId: z.string(),
-    amount: z.coerce.number(),
-    status: z.enum(['pending', 'paid']),
+    customerId: z.string({
+        invalid_type_error: 'Please select a customer.',
+    }),
+    amount: z.coerce
+        .number()
+        .gt(0, { message: 'Please enter an amount greater than $0.' }),
+    status: z.enum(['pending', 'paid'], {
+        invalid_type_error: 'Please select an invoice status.',
+    }),
     date: z.string(),
 });
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoice(prevState: any, formData: FormData) {
+export type State = {
+    errors?: {
+        customerId?: string[];
+        amount?: string[];
+        status?: string[];
+    };
+    message?: string | null;
+};
+
+export async function createInvoice(prevState: State, formData: FormData) {
     const validatedFields = CreateInvoice.safeParse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
@@ -90,18 +105,9 @@ export async function updateInvoice(id: string, prevState: any, formData: FormDa
     redirect('/dashboard/invoices');
 }
 
-export async function deleteInvoice(id: string, formData: FormData) {
-    throw new Error('Failed to Delete Invoice');
-    try {
-
-        await sql`DELETE FROM invoices WHERE id = ${id}`;
-        revalidatePath('/dashboard/invoices');
-    } catch (error) {
-        console.error('Error deleting invoice:', error);
-        return {
-            message: 'Database Error: Failed to Delete Invoice.',
-        };
-    }
+export async function deleteInvoice(id: string) {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    revalidatePath('/dashboard/invoices');
 }
 
 export async function authenticate(
